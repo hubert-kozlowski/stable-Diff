d40d1fa311f7440f64b3ae9d550bc5ad44e0c525
blinno <business.kozlowskihubert@gmail.com>
2023-10-22 15:07:06 +0100

    button

--- app.py
+++ app.py
@@ -17,14 +17,19 @@ app.title("Stable Diffusion Demo")
 app.resizable(False, False)
 
 # Create prompt box
-prompt = ctk.CTkEntry(height=50, width=580,text_color="black", fg_color="white", master=app)
+prompt = ctk.CTkEntry(height=50, width=580, text_color="black", fg_color="white", master=app)
 # Place prompt box
 prompt.place(x=10, y=10)
 
 # Create main
-lmain = ctk.CTkLabel(height=500, width=580, master=app)
+lmain = ctk.CTkLabel(height=500, width=580, fg_color="blue", master=app) # (Blue is a placeholder colour)
 # Place main
 lmain.place(x=10, y=60)
+
+# Create button
+button = ctk.CTkButton(height=30, width=130, text="Submit", master=app)
+# Place button
+button.place(x=235, y=570)
 
 app.mainloop()
                       

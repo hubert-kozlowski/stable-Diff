07797a02abd82326a2eb29a22ae5450463a95206
blinno <business.kozlowskihubert@gmail.com>
2023-10-22 15:05:25 +0100

    lmain

--- app.py
+++ app.py
@@ -10,14 +10,21 @@ from torch import autocast
 # Diffuser imports
 from diffusers import StableDiffusionPipeline
 
-
+# Create app
 app = tk.Tk()
 app.geometry("600x600")
 app.title("Stable Diffusion Demo")
 app.resizable(False, False)
 
+# Create prompt box
 prompt = ctk.CTkEntry(height=50, width=580,text_color="black", fg_color="white", master=app)
+# Place prompt box
 prompt.place(x=10, y=10)
+
+# Create main
+lmain = ctk.CTkLabel(height=500, width=580, master=app)
+# Place main
+lmain.place(x=10, y=60)
 
 app.mainloop()
                       
